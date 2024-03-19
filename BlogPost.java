0554977017eda5f75145679d23cf3494840e0b1e
NOULURI  RAJASHRI <110009081+RAJASHRINOULURI@users.noreply.github.com>
2024-03-19 19:29:01 +0530

    Add files via upload

---
+++ BlogPost.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author RAJASHRI
+ */
+public class BlogPost {
+    private String title;
+    private String description;
+
+    // Constructor, getters, and setters
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+}
